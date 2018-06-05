92f47cd82ba10c6b031ac623b35a2fc04f224b6e
kimura.arito <kimura.arito@tsd-201511-0004>
2018-06-05 16:44:18 +0900

    UpdateServletのwebservlet修正

--- src/abs3/UpdateServlet.java
+++ src/abs3/UpdateServlet.java
@@ -7,11 +7,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet("/upadate.html")
+@WebServlet("/update.html")
 public class UpdateServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+
 		getServletContext().getRequestDispatcher("/WEB-INF/update.jsp")
 		.forward(req, resp);
 	}
